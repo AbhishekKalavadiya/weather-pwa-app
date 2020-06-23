f60a5b69a988cd0fc164064845402554b93762b5
Abhishek Kalavadiya <51313689+AbhishekKalavadiya@users.noreply.github.com>
2020-06-23 05:05:19 -0700

    Updates

---
+++ precache-manifest.1cc5278eb0173965c5939d065cc1bce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ae094e77ec2a4f6418af5ef81218ba",
+    "url": "/weather-pwa-app/index.html"
+  },
+  {
+    "revision": "d49aa25337cc38eb4bcd",
+    "url": "/weather-pwa-app/static/css/main.15fb3d4c.chunk.css"
+  },
+  {
+    "revision": "ba893f69efea1e46efbb",
+    "url": "/weather-pwa-app/static/js/2.3e9262e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-pwa-app/static/js/2.3e9262e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d49aa25337cc38eb4bcd",
+    "url": "/weather-pwa-app/static/js/main.3a02fe8b.chunk.js"
+  },
+  {
+    "revision": "01b062b3d7278ee477ee",
+    "url": "/weather-pwa-app/static/js/runtime-main.c71a18ba.js"
+  }
+]);
